d52f4cfd9b5b1788c30b4101c4e6c331fb6aad87
N3zar3c <128186552+n3zar3c@users.noreply.github.com>
2023-12-28 17:02:56 +0100

    Initial Project Structure

---
+++ src/main/java/com/vtrk/videotracker/VideoTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.vtrk.videotracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vtrk/videotracker/VideoTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtrk.videotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
