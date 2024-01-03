3e387b3e223ca27bf53a748a08b30d1fe3485856
N3zar3c <plrgnn02h23d122w@studenti.unical.it>
2024-01-03 05:11:14 +0100

    Test Kitsu API

--- src/test/java/com/vtrk/videotracker/VideoTrackerApplicationTests.java
+++ src/test/java/com/vtrk/videotracker/VideoTrackerApplicationTests.java
@@ -1,13 +1,27 @@
 package com.vtrk.videotracker;
 
+import com.vtrk.videotracker.API.Kitsu;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Kitsu.class)
 class VideoTrackerApplicationTests {
 
+    @Autowired
+    Kitsu kitsu;
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void KitsuAPITest() {
+        System.out.println("Kitsu API Test");
+        try {
+            System.out.println("JSON API Response: " + kitsu.searchAnime("bleach"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
