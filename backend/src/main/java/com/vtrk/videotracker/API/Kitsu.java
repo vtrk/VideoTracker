b95127f663c48d93021b4aed39c330bfbeb23d94
N3zar3c <plrgnn02h23d122w@studenti.unical.it>
2024-01-05 00:48:48 +0100

    Added Kitsu API functions getAnimeById getTrendingAnime searchAnimeFilters

--- backend/src/main/java/com/vtrk/videotracker/API/Kitsu.java
+++ backend/src/main/java/com/vtrk/videotracker/API/Kitsu.java
@@ -3,6 +3,7 @@ package com.vtrk.videotracker.API;
 import com.vtrk.videotracker.utils.Properties;
 
 import java.io.IOException;
+import java.util.Map;
 
 /**
  Kitsu API class
@@ -40,6 +41,50 @@ public class Kitsu extends API {
      */
     public String searchAnime(String query) throws IOException {
         return makeRequest("/anime?filter%5Btext%5D=" + query, "GET", new java.util.HashMap<>() {{
+            put("Accept", "application/vnd.api+json");
+            put("Content-Type", "application/vnd.api+json");
+            put("User-Agent", "VideoTracker/" + Properties.getInstance().getProperty("VERSION"));
+        }});
+    }
+
+    /**
+        Search anime by queries and filters
+        @param filtersQueries map of filters and queries
+        @return JSON response
+        @throws IOException if connection fails
+     */
+    public String searchAnimeFilters(Map<String, String> filtersQueries) throws IOException {
+        StringBuilder query = new StringBuilder();
+        for(String key : filtersQueries.keySet())
+            query.append("filter%5B").append(key).append("%5D=").append(filtersQueries.get(key)).append("&");
+        return makeRequest("/anime?" + query, "GET", new java.util.HashMap<>() {{
+            put("Accept", "application/vnd.api+json");
+            put("Content-Type", "application/vnd.api+json");
+            put("User-Agent", "VideoTracker/" + Properties.getInstance().getProperty("VERSION"));
+        }});
+    }
+
+    /**
+        Get anime by ID
+        @param id anime ID
+        @return JSON response
+        @throws IOException if connection fails
+     */
+    public String getAnimeById(String id) throws IOException {
+        return makeRequest("/anime/" + id, "GET", new java.util.HashMap<>() {{
+            put("Accept", "application/vnd.api+json");
+            put("Content-Type", "application/vnd.api+json");
+            put("User-Agent", "VideoTracker/" + Properties.getInstance().getProperty("VERSION"));
+        }});
+    }
+
+    /**
+        Get trending anime
+        @return JSON response
+        @throws IOException if connection fails
+     */
+    public String getTrendingAnime() throws IOException {
+        return makeRequest("/trending/anime", "GET", new java.util.HashMap<>() {{
             put("Accept", "application/vnd.api+json");
             put("Content-Type", "application/vnd.api+json");
             put("User-Agent", "VideoTracker/" + Properties.getInstance().getProperty("VERSION"));
--- backend/src/test/java/com/vtrk/videotracker/VideoTrackerApplicationTests.java
+++ backend/src/test/java/com/vtrk/videotracker/VideoTrackerApplicationTests.java
@@ -2,14 +2,13 @@ package com.vtrk.videotracker;
 
 import com.vtrk.videotracker.API.Kitsu;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest(classes = Kitsu.class)
 class VideoTrackerApplicationTests {
-
-    @Autowired
-    Kitsu kitsu;
     @Test
     void contextLoads() {
     }
@@ -17,11 +16,19 @@ class VideoTrackerApplicationTests {
     @Test
     void KitsuAPITest() {
         System.out.println("Kitsu API Test");
+        assertEquals("Kitsu", Kitsu.getInstance().getApiName());
         try {
-            System.out.println("JSON API Response: " + kitsu.searchAnime("bleach"));
+            System.out.println("Testing searchAnime\nJSON API Response: " + Kitsu.getInstance().searchAnime("bleach"));
+            System.out.println("Testing getAnimeById\nJSON API Response: " + Kitsu.getInstance().getAnimeById("1"));
+            System.out.println("Testing trendingAnime\nJSON API Response: " + Kitsu.getInstance().getTrendingAnime());
+            System.out.println("Testing searchAnimeFilters\nJSON API Response: " + Kitsu.getInstance().searchAnimeFilters(new java.util.HashMap<>() {{
+                put("text", "bleach");
+                put("categories", "Action");
+            }}));
         } catch (Exception e) {
-            e.printStackTrace();
+            fail(e.getMessage());
         }
+        System.out.println("Kitsu API Test Passed");
     }
 
 }
