7a6ed97d750accd92a68cbb8cfacc4253aa50b49
N3zar3c <plrgnn02h23d122w@studenti.unical.it>
2024-01-06 00:42:00 +0100

    Added TMDBAPITest

--- backend/src/test/java/com/vtrk/videotracker/VideoTrackerApplicationTests.java
+++ backend/src/test/java/com/vtrk/videotracker/VideoTrackerApplicationTests.java
@@ -1,6 +1,8 @@
 package com.vtrk.videotracker;
 
 import com.vtrk.videotracker.API.Kitsu;
+import com.vtrk.videotracker.API.TMDB;
+import com.vtrk.videotracker.utils.Properties;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -29,6 +31,28 @@ class VideoTrackerApplicationTests {
             fail(e.getMessage());
         }
         System.out.println("Kitsu API Test Passed");
+    }
+
+    @Test
+    void TMDBAPITest() {
+        System.out.println("TMDB API Test");
+        if(Properties.getInstance().getProperty("TMDB_API_KEY").equals(""))
+            fail("TMDB_API_KEY not found in application.properties");
+        assertEquals("TMDB", com.vtrk.videotracker.API.TMDB.getInstance().getApiName());
+        try {
+            System.out.println("Testing searchMovie\nJSON API Response: " + TMDB.getInstance().searchMovie("spider man"));
+            System.out.println("Testing searchMovie (with year)\nJSON API Response: " + TMDB.getInstance().searchMovie("spider man", "2021"));
+            System.out.println("Testing getMovieById\nJSON API Response: " + TMDB.getInstance().getMovieById("634649"));
+            System.out.println("Testing searchTV\nJSON API Response: " + TMDB.getInstance().searchTV("the office"));
+            System.out.println("Testing searchTV (with year)\nJSON API Response: " + TMDB.getInstance().searchTV("the office", "2005"));
+            System.out.println("Testing getTVById\nJSON API Response: " + TMDB.getInstance().getTVById("2316"));
+            System.out.println("Testing getAllTrending\nJSON API Response: " + TMDB.getInstance().getAllTrending());
+            System.out.println("Testing getTrendingMovies\nJSON API Response: " + TMDB.getInstance().getTrendingMovies());
+            System.out.println("Testing getTrendingTV\nJSON API Response: " + TMDB.getInstance().getTrendingTV());
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+        System.out.println("TMDB API Test Passed");
     }
 
 }
