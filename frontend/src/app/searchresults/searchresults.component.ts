09caa9965cc7c02e799b3bfe424998ad6525519b
Marco Martino <59832916+Marco-Maker@users.noreply.github.com>
2024-01-05 17:15:34 +0100

    Starting the frontend pages

---
+++ frontend/src/app/searchresults/searchresults.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-searchresults',
+  standalone: true,
+  imports: [],
+  templateUrl: './searchresults.component.html',
+  styleUrl: './searchresults.component.css'
+})
+export class SearchresultsComponent {
+
+}
