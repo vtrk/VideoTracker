0dbe601f0a4034754b60c024cd7d9ff861193451
N3zar3c <plrgnn02h23d122w@studenti.unical.it>
2024-01-06 01:55:28 +0100

    removed un-thrown IOException

--- backend/src/main/java/com/vtrk/videotracker/VideoTrackerApplication.java
+++ backend/src/main/java/com/vtrk/videotracker/VideoTrackerApplication.java
@@ -16,7 +16,7 @@ public class VideoTrackerApplication {
     public static API_Manager API_MANAGER;
     static Logger logger = Logger.getLogger(VideoTrackerApplication.class.getName());
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         if (Properties.getInstance().getProperty("API").equals("TMDB")) {
             API_MANAGER = new TMDBManager();
         } else if (Properties.getInstance().getProperty("API").equals("Kitsu")) {
