6e3bd250dc7ee5facc22301da29c430eaad72419
N3zar3c <plrgnn02h23d122w@studenti.unical.it>
2024-01-06 01:51:19 +0100

    Initialize static API_MANAGER at server start

--- backend/src/main/java/com/vtrk/videotracker/VideoTrackerApplication.java
+++ backend/src/main/java/com/vtrk/videotracker/VideoTrackerApplication.java
@@ -1,5 +1,8 @@
 package com.vtrk.videotracker;
 
+import com.vtrk.videotracker.API.Manager.API_Manager;
+import com.vtrk.videotracker.API.Manager.KitsuManager;
+import com.vtrk.videotracker.API.Manager.TMDBManager;
 import com.vtrk.videotracker.utils.Properties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,9 +13,17 @@ import java.util.logging.Logger;
 @SpringBootApplication
 public class VideoTrackerApplication {
 
+    public static API_Manager API_MANAGER;
     static Logger logger = Logger.getLogger(VideoTrackerApplication.class.getName());
 
     public static void main(String[] args) throws IOException {
+        if (Properties.getInstance().getProperty("API").equals("TMDB")) {
+            API_MANAGER = new TMDBManager();
+        } else if (Properties.getInstance().getProperty("API").equals("Kitsu")) {
+            API_MANAGER = new KitsuManager();
+        } else {
+            throw new RuntimeException("API not found");
+        }
         SpringApplication.run(VideoTrackerApplication.class, args);
 
         logger.info("Version: " + Properties.getInstance().getProperty("VERSION"));
