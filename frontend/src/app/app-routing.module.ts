4b72cc13ecb3932e79df13c198744887cfece325
N3zar3c <plrgnn02h23d122w@studenti.unical.it>
2024-01-07 16:45:33 +0100

    routing fixed

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContentComponent } from './content/content.component';
 import {UserListComponent} from "./user-list/user-list.component";
+import { LoginComponent } from "./login/login.component";
 
 
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'content', component: ContentComponent },
-  { path: 'user-list', component: UserListComponent }
+  { path: 'user-list', component: UserListComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
