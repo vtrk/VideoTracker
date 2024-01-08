1154af44e13b54d7fab8c13272249ecbad28e3f0
N3zar3c <plrgnn02h23d122w@studenti.unical.it>
2024-01-08 22:27:04 +0100

    common strings

---
+++ frontend/src/app/strings.ts
@@ -0,0 +1,12 @@
+export const strings = {
+    KITSU: "Kitsu",
+    TMDB: "TMDB",
+    TRENDING_ERROR: "Error loading trending content.",
+    SEARCH_ERROR: "Error loading search results.",
+    SEARCH_NO_RESULTS: "No results found.",
+    SEARCH_RESULTS: "Search Results",
+    trending: "Trending",
+    anime: "anime",
+    movie: "movie",
+    tv: "tv",
+};
