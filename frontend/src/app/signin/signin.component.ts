493ed69609c7a8afb941e9f60ca8e4ec3e10b6ab
Marco Martino <59832916+Marco-Maker@users.noreply.github.com>
2024-01-08 12:56:53 +0100

    Log-in e Sign-in
    They have functions. Just untill we have a DB, they print the answers on the console.

--- frontend/src/app/signin/signin.component.ts
+++ frontend/src/app/signin/signin.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
 export class SigninComponent {
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
 
+  signin(){
+    console.log(this.email, this.password, this.confirmPassword);
+  }
 }
