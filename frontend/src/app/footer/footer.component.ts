658afa79b547cc65be7d50bab98a4dcd2cc21100
N3zar3c <plrgnn02h23d122w@studenti.unical.it>
2024-01-08 23:06:07 +0100

    added github logo via fontawesome component

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
     imports: [
-        RouterLink
+        RouterLink,
+        FontAwesomeModule
     ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  faGithub = faGithub;
 }
