3ce2723000632d179cef81fce8638958c29bd9fa
N3zar3c <plrgnn02h23d122w@studenti.unical.it>
2024-01-08 03:33:40 +0100

    about gets api and server version

--- frontend/src/app/about/about.component.ts
+++ frontend/src/app/about/about.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { ServerAPIResponse } from '../data-structures';
 
 @Component({
   selector: 'app-about',
@@ -7,6 +11,19 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit{
+  title = 'about';
+  serverAPI: string;
+  serverVersion: string;
 
+  ngOnInit(): void {
+    this.ref.detectChanges();
+  }
+
+  constructor(private ref:ChangeDetectorRef, private client: HttpClient) {
+    this.client.get<ServerAPIResponse>(environment.API_URL + '/api').subscribe(data => {
+      this.serverAPI = "Server API: " + data.API;
+      this.serverVersion = "Server Version: " + data.Version;
+    });
+  }
 }
