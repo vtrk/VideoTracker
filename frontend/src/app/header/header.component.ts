39cb51a24f36876ef709a73e4be27bbbab516ff8
N3zar3c <plrgnn02h23d122w@studenti.unical.it>
2024-01-08 03:33:55 +0100

    testing search

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {SearchService} from "../search.service";
+import { HttpClient } from '@angular/common/http';
+import { FormControl, FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
     imports: [
-        RouterLink
+        RouterLink,
+        FormsModule,
+        ReactiveFormsModule
     ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
+  input: FormControl;
+
+  private search: SearchService;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void{
+    this.input = new FormControl('');
+  }
+
+
+  onSubmit(){
+    //alert(this.input.value);
+    console.log(this.search.sendSearchQuery(this.input.value, 'anime', new Map<string, string>))
+  }
 }
