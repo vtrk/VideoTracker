13edc0cebe6107600a93c26d79d9c221997df2a8
N3zar3c <plrgnn02h23d122w@studenti.unical.it>
2024-01-09 02:51:47 +0100

    searchresults route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import {AboutComponent} from "./about/about.component";
 import { LoginComponent } from "./login/login.component";
 import { SigninComponent } from "./signin/signin.component";
 import { SearchService } from "./search.service";
+import { SearchresultsComponent } from "./searchresults/searchresults.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
@@ -16,7 +17,9 @@ export const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'user-list', component: UserListComponent },
   { path: 'signin', component: SigninComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'search/:searchQuery', component: SearchresultsComponent },
+  { path: 'search', redirectTo: '/home', pathMatch: 'full' } // Redirects to home if no search query is given.
 ];
 
 @NgModule({
