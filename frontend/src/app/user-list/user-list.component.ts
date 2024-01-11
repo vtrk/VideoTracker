80cb44c33a8b7accff87d40907583936fde6da77
nichael <gduncl02m08d086a@studenti.unical.it>
2024-01-11 11:53:25 +0100

    Searchbar aesthetic + user-list themes

--- frontend/src/app/user-list/user-list.component.ts
+++ frontend/src/app/user-list/user-list.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import {NgClass} from "@angular/common";
+import {ThemeService} from "../theme.service";
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
 export class UserListComponent {
-
+  constructor(public themeService: ThemeService) {
+  }
 }
