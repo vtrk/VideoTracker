9f83f6451237a507ccad328bacbfe7efb4c698fc
nichael <gduncl02m08d086a@studenti.unical.it>
2024-01-13 16:07:18 +0100

    About removed

--- frontend/src/app/mailbox/mailbox.component.ts
+++ frontend/src/app/mailbox/mailbox.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import {NgClass} from "@angular/common";
+import {ThemeService} from "../theme.service";
 
 @Component({
   selector: 'app-mailbox',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './mailbox.component.html',
   styleUrl: './mailbox.component.css'
 })
 export class MailboxComponent {
-
+  constructor(public themeService: ThemeService) {
+  }
 }
