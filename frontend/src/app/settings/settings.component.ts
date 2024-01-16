3142cd49e03e71c3c824db964f40ae6f34830c23
Marco Martino <59832916+Marco-Maker@users.noreply.github.com>
2024-01-16 15:56:19 +0100

    Implementation to use Settings

--- frontend/src/app/settings/settings.component.ts
+++ frontend/src/app/settings/settings.component.ts
@@ -17,10 +17,10 @@ import {ServerApiService} from "../services/server-api.service";
   styleUrl: './settings.component.css'
 })
 export class SettingsComponent {
-  old_email: string = '';
+  error_email: boolean = true;
   new_email: string = '';
   confirm_email: string = '';
-  old_password: string = '';
+  error_password: boolean = true;
   new_password: string = '';
   confirm_password: string = '';
   new_username: string = '';
@@ -28,18 +28,36 @@ export class SettingsComponent {
   constructor(private api: ServerApiService,private cookieService: CookieService,private authService: AuthenticationService, public themeService : ThemeService) {}
 
   change_password(){
-    console.log(this.old_password, this.new_password, this.confirm_password);
+    console.log(this.new_password, this.confirm_password);
     //qui bisogna chiama il server per cambiare la password
+    if(this.new_password == this.confirm_password){
+      this.api.changePassword(this.cookieService.get('id_user'),this.new_password);
+    }else{
+      this.error_password = true;
+    }
   }
 
   change_email(){
-    console.log(this.old_email, this.new_email, this.confirm_email);
+    console.log(this.new_email, this.confirm_email);
     //qui bisogna chiama il server per cambiare l'email
+    if(this.new_email == this.confirm_email){
+      this.api.changePassword(this.cookieService.get('id_user'),this.new_email);
+    }else{
+      this.error_email = true;
+    }
   }
 
   change_username(){
     console.log(this.new_username);
     //qui bisogna chiama il server per cambiare l'username
+    this.api.changePassword(this.cookieService.get('id_user'),this.new_username);
+  }
+
+  show_message_password(){
+    return this.error_password;
+  }
+  show_message_email(){
+    return this.error_email;
   }
 
 }
