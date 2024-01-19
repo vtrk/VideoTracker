064a15414c4cc7bef4e0a3fe8dd4d9a93354f0e9
N3zar3c <plrgnn02h23d122w@studenti.unical.it>
2024-01-19 04:14:51 +0100

    footer rework

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import {ThemeService} from "../services/theme/theme.service";
-import {NgClass} from "@angular/common";
+import { faX } from '@fortawesome/free-solid-svg-icons';
+import { ThemeService } from "../services/theme/theme.service";
+import { NgClass } from "@angular/common";
+import { strings } from '../utils/strings';
+import { ServerApiService } from '../services/server-api/server-api.service';
 
 @Component({
   selector: 'app-footer',
@@ -18,6 +21,18 @@ import {NgClass} from "@angular/common";
 })
 export class FooterComponent {
   faGithub = faGithub;
-  constructor(public themeService: ThemeService) {
+  faX = faX;
+  client_version = strings.CLIENT_VERSION;
+  server_version = '';
+  server_api = '';
+
+
+  constructor(public themeService: ThemeService, private server: ServerApiService) {
+    this.server.getServerInfoObservable().subscribe(
+      (data) => {
+        this.server_version = data.Version;
+        this.server_api = data.API;
+      }
+    );
   }
 }
