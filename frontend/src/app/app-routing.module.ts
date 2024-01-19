d920ac7e31d1f0b5faa66695a5afcb6dc6e7e816
N3zar3c <plrgnn02h23d122w@studenti.unical.it>
2024-01-19 02:27:22 +0100

    moved auth.guard to guard folder

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from "./auth.guard";
+import { AuthGuard } from "./guard/auth.guard";
 
 import { HomeComponent } from './home/home.component';
 import { SettingsComponent } from './settings/settings.component';
--- frontend/src/app/guard/auth.guard.ts
+++ frontend/src/app/guard/auth.guard.ts
@@ -1,6 +1,6 @@
 import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
-import { AuthenticationService } from './services/authentication-service/authentication.service';
+import { AuthenticationService } from '../services/authentication-service/authentication.service';
 
 
 @Injectable({
