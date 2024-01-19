82d7b1bca8103e02b61bb19ed54b2adc1c9d23eb
N3zar3c <plrgnn02h23d122w@studenti.unical.it>
2024-01-19 05:04:05 +0100

    strings cleanup

--- frontend/src/app/utils/strings.ts
+++ frontend/src/app/utils/strings.ts
@@ -1,8 +1,10 @@
 export const strings = {
-    about_serverVersion: "Server Version: ",
-    about_serverAPI: "Server API: ",
+    // Info
     KITSU: "Kitsu",
     TMDB: "TMDB",
+    CLIENT_VERSION: "1.0.0",
+    
+    // Server side errors
     API_ERROR: "Error fetching API data",
     TRENDING_ERROR: "Error loading trending content",
     SEARCH_ERROR: "Error loading search results",
@@ -11,25 +13,28 @@ export const strings = {
     PROFILE_ERROR: "Error loading profile",
     SEARCH_NO_RESULTS: "No results found.",
     SEARCH_RESULTS: "Search Results",
+    NOTIFICATIONS_ERROR: "Error loading notifications",
+    ERROR_NOTIFICATIONS_DELETE: "Error deleting notification",
+    SERVER_ERROR: "Server error: The server is either down or not responding",
+    ERROR_CHANGE_USERNAME: "Error changing username",
+    ERRIR_CHANGE_EMAIL: "Error changing email",
+
+    // General
     trending: "Trending",
     anime: "anime",
     movie: "movie",
     tv: "tv",
     movie_year: "primary_release_year",
     tv_year: "first_air_date_year",
+
     TMDB_poster_url: "https://image.tmdb.org/t/p/w500",
+
+    // Form errors
     email_in_use: "Email already in use",
-    
     registration_error: "Error registering user",
     password_mismatch: "Passwords do not match",
     login_error: "Error logging in",
     wrong_credentials: "Wrong email or password",
-    NOTIFICATIONS_ERROR: "Error loading notifications",
-    ERROR_NOTIFICATIONS_DELETE: "Error deleting notification",
-    SERVER_ERROR: "Server error: The server is either down or not responding",
     WRONG_PASSWORD: "Wrong password",
-    ERROR_CHANGE_USERNAME: "Error changing username",
     EMAILS_DONT_MATCH: "Emails don't match",
-    ERRIR_CHANGE_EMAIL: "Error changing email",
-    CLIENT_VERSION: "1.0.0",
 };
