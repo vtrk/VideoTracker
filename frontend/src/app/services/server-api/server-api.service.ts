4581c8d901f9a86c31cfa8c9851184ab7e9698cb
N3zar3c <plrgnn02h23d122w@studenti.unical.it>
2024-01-19 04:53:58 +0100

    fixed request body for movies

--- frontend/src/app/services/server-api/server-api.service.ts
+++ frontend/src/app/services/server-api/server-api.service.ts
@@ -484,6 +484,7 @@ export class ServerApiService {
         'Accept': 'text/plain'
       }
     };
+    if(duration == undefined) duration = '-1'; // If the duration is undefined, set it to -1. (happens with movies)
     let JSONBody = {
       id_user: this.cookieService.get('id_user'),
       id_content: id_content + '_' + type,
