4d4a7aaf89785cf4a7070b7904bdfc34c007b3fa
N3zar3c <plrgnn02h23d122w@studenti.unical.it>
2024-01-20 17:30:05 +0100

    Fixed exception "trailing junk after numeric literal at or near"

--- backend/src/main/java/com/vtrk/videotracker/Database/Dao/Postgres/ReviewDaoPostgres.java
+++ backend/src/main/java/com/vtrk/videotracker/Database/Dao/Postgres/ReviewDaoPostgres.java
@@ -123,7 +123,8 @@ public class ReviewDaoPostgres implements ReviewDao, Subject {
     @Override
     public boolean exists(int id_user, String id_content) {
         try {
-            String query = "SELECT * FROM review WHERE id_user = " + id_user + " AND id_content = "+id_content+";";
+            System.out.println(id_content);
+            String query = "SELECT * FROM review WHERE id_user = " + id_user + " AND id_content = '"+id_content+"';";
             Statement st = connection.createStatement();
             ResultSet rs = st.executeQuery(query);
             if(!rs.isBeforeFirst())
